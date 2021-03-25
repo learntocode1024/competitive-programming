fe9f5b164f2b63c61b2344df29e6a653ccc735ff
misaka18931 <misaka18931@outlook.com>
2021-03-26 00:47:51 +0800

    div3

---
+++ problem/codeforces-round-710-div-3/G-maximize-the-remaining-string/main.cpp
@@ -0,0 +1,32 @@
+// Original Author: misaka18931
+// Date: 03-18-21
+// tag:
+// 
+
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+typedef long long LL;
+typedef unsigned long long ULL;
+const LL mod = 1e9 + 7;
+#define MX
+
+
+void solve() {
+  int n;
+  cin >> n;
+  
+}
+
+int main() {
+  cin.tie(NULL);
+  std::ios::sync_with_stdio(false);
+  int T = 1;
+  cin >> T;
+  while (T--) {
+    solve();
+  }
+  return 0;
+}
