6007a467ebc23f764ee74610ea0c78e627898120
misaka18931 <misaka18931@outlook.com>
2021-03-29 08:34:48 +0800

    problem solving

--- problem/codeforces-round-710-div-3/F-triangular-paths/main.cpp
+++ problem/codeforces-round-710-div-3/F-triangular-paths/main.cpp
@@ -11,13 +11,34 @@ using namespace std;
 typedef long long LL;
 typedef unsigned long long ULL;
 const LL mod = 1e9 + 7;
-#define MX
+#define MX 200005
+#define r first
+#define c second
+typedef pair<int, int> pt;
 
+pt a[MX];
 
 void solve() {
   int n;
   cin >> n;
-  
+  for (int i = 0; i < n; ++i)
+    cin >> a[i].r;
+  for (int i = 0; i < n; ++i)
+    cin >> a[i].c;
+  sort (a, a + n, [] (auto a, auto b) { return a.r < b.r; });
+  pt curr = make_pair(1, 1);
+  int ans = 0;
+  for (int i = 0; i < n; ++i) {
+    const pt &to = a[i];
+    if (curr.r - curr.c == to.r - to.c)
+      ans += (to.r - curr.r) * (~(curr.c + curr.r) & 1);
+    else {
+      if (~(curr.c + curr.r) & 1) curr.r++;
+      ans += (((to.r - to.c) - (curr.r - curr.c)) + 1) / 2;
+    }
+    curr = to;
+  }
+  cout << ans << endl;
 }
 
 int main() {
