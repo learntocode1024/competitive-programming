b5496bde664b9a5b9a96bf98aa3e6ad511a576ac
misaka18931 <misaka18931@outlook.com>
2021-06-23 17:26:12 +0800

    todo

---
+++ luogu/P2824.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <iostream>
+#define MX 100005
+
+int c[MX];
+int a[MX << 2];
+int f[MX << 2];
+
+void ins(int p, int l, int r, int s, int t) {
+  
+}
+
+int main() {
+  
+  return 0;
+}
