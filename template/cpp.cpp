ebbfe632a6421bc96db59c52259aaa34d1be33b6
misaka18931 <misaka18931@protonmail.com>
2021-08-03 22:24:31 +0800

    simplified

--- template/cpp.cpp
+++ template/cpp.cpp
@@ -21,37 +21,6 @@
 using namespace std;
 
 /********************************** buffer IO *********************************/
-#ifdef MASSIVE_INPUT
-namespace IO {
-template <int size> struct io {
-  char ibuf[size], obuf[size];
-  char *o, *o1, *p, *p1;
-  io() : o(ibuf), o1(ibuf), p(obuf), p1(obuf + size){};
-  ~io() { fwrite(obuf, 1, p - obuf, stdout); }
-  inline char gc() {
-    return o == o1 && (o1 = (o = ibuf) + fread(ibuf, 1, size, stdin)), *o++;
-  };
-  inline void pc(char ch) {
-    p == p1 &&fwrite(obuf, 1, p - obuf, stdout) && (p = obuf), *p++ = ch;
-  }
-};
-io<1 << 20> f;
-inline int read() {
-  int x = 0, u = 1;
-  char ch = f.gc();
-  while (ch < '0' || ch > '9') {
-    if (ch == '-')
-      u = -1;
-    ch = f.gc();
-  }
-  while (ch >= '0' && ch <= '9') {
-    x = (x << 1) + (x << 3) + ch - '0';
-    ch = f.gc();
-  }
-  return x * u;
-}
-} // namespace IO
-#else
 namespace IO {
 char in[1 << 23]; // sizeof in varied in problem
 char const *o;
@@ -61,7 +30,7 @@ void init_in() {
   in[fread(in, 1, sizeof(in) - 4, stdin)] = 0; // set 0 at the end of buffer.
 }
 
-int read() {
+int rd() {
   unsigned u = 0, s = 0;
 
   while (*o && *o <= 32)
@@ -77,8 +46,15 @@ int read() {
 
   return static_cast<int>((u ^ s) + !!s);
 }
+
+char *rdstr(char *s) {
+  while (*o && *o <= 32)
+    ++o;
+  while (*o > 32)
+    *s++ = *o++;
+  return s;
+}
 } // namespace IO
-#endif
 
 /********************************* macros *************************************/
 typedef long long i64;
@@ -93,7 +69,7 @@ template <typename T> void chkmax(T &a, const T &b) { a = max(a, b); }
 template <typename T> void chkmin(T &a, const T &b) { a = min(a, b); }
 
 /*********************************** solution *********************************/
-using IO::read = rd;
+using IO::rd;
 #define MX
 
 void solve() {
@@ -101,13 +77,11 @@ void solve() {
 }
 
 int main() {
-#ifndef MASSIVE_INPUT
   IO::init_in();
-#endif
 #ifdef MULTI
-  int T = IO::read();
+  int T = IO::rd();
   while (T--)
-    solve(), T &&clear();
+    solve(), T && (clear(), 1);
 #else
   solve();
 #endif
