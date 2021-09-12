7ef96e55baead3052b4f4c517719700afeb75e30
misaka18931 <misaka18931@protonmail.com>
2021-09-13 00:58:54 +0800

    Codeforces Global Round 16

---
+++ codeforces/codeforces-global-round-16/c.cpp
@@ -0,0 +1,127 @@
+/**********************************************************************
+ * This file is the c++ solution to a particular CP problem written by
+ * misaka18931 and was hosted on GitHub Repository below:
+ * URL: https://github.com/misaka18931/competitive-programming
+ *
+ * Original Author: misaka18931
+ * Date:
+ * Algorithm:
+ * Difficulty:
+ *
+ *********************************************************************/
+
+#include <algorithm>
+#include <cctype>
+#include <climits>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+/********************************** buffer IO *********************************/
+namespace IO {
+char in[1 << 24];  // sizeof in varied in problem
+char const *o;
+void init_in() {
+  o = in;
+  in[fread(in, 1, sizeof(in) - 4, stdin)] = 0;  // set 0 at the end of buffer.
+}
+int rd() {
+  unsigned u = 0, s = 0;
+  while (*o && *o <= 32) ++o;  // skip whitespaces...
+  if (*o == '-')
+    s = ~s, ++o;
+  else if (*o == '+')
+    ++o;  // skip sign
+  while (*o >= '0' && *o <= '9')
+    u = (u << 3) + (u << 1) + (*o++ - '0');  // u * 10 = u * 8 + u * 2 :)
+  return static_cast<int>((u ^ s) + !!s);
+}
+char *rdstr(char *s) {
+  while (*o && *o <= 32) ++o;
+  while (*o > 32) *s++ = *o++;
+  *s = 0;
+  return s;
+}
+}  // namespace IO
+
+/********************************* utility ************************************/
+typedef long long i64;
+typedef unsigned long long u64;
+typedef unsigned int u32;
+typedef pair<int, int> pii;
+#define pb(x) push_back(x)
+#define mkp(x, y) make_pair(x, y)
+#define fi first
+#define se second
+#define FOR(x, y, z) for (int x = y; x < z; ++x)  // always [y, z)
+#define ROF(x, y, z) for (int x = z - 1; x >= y; --x)
+template <typename T>
+void chkmax(T &a, const T &b) {
+  a = max(a, b);
+}
+template <typename T>
+void chkmin(T &a, const T &b) {
+  a = min(a, b);
+}
+pii operator+(const pii &a, const pii &b) {
+  return mkp(a.fi + b.fi, a.se + b.se);
+}
+
+/*********************************** solution *********************************/
+using IO::rd;
+#define MULTI
+const int N = 100005;
+char s1[N], s2[N];
+bool vis[N];
+
+void solve() {
+  int n = rd();
+  IO::rdstr(s1), IO::rdstr(s2);
+  FOR(i, 0, n) vis[i] = 0;
+  int ans = 0;
+  FOR(i, 0, n) {
+    if (vis[i]) continue;
+    if (s1[i] != s2[i]) vis[i] = 1, ans += 2;
+    else {
+      if (s1[i] == '1') {
+        if (i && !vis[i - 1] && s1[i - 1] == '0') {
+          vis[i - 1] = vis[i] = 1;
+          ans += 2;
+        } else if (i + 1 < n && s1[i + 1] == '0' && s2[i + 1] == '0') {
+          vis[i] = vis[i + 1] = 1;
+          ans += 2;
+        }
+      }
+    }
+  }
+  FOR(i, 0, n) {
+    if (!vis[i] && s1[i] == '0' && s2[i] == '0') ++ans;
+    // cout << vis[i] << ' ';
+  }
+  cout << ans << '\n';
+}
+
+int main() {
+  IO::init_in();
+#ifdef MULTI
+  int T = IO::rd();
+  while (T--) solve();
+#else
+  solve();
+#endif
+  return 0;
+}
+/*
+ * checklist:
+ * - IO buffer size
+ * - potential out-of-bound Errors
+ * - inappropriate variable type
+ * - potential Arithmetic Error
+ * - potential Arithmetic Overflow
+ * - typo / logical flaws
+ * - clean-up on multiple test cases
+ * - sufficient stress tests / random data tests
+*/
