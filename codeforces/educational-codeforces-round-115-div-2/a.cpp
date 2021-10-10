b3dcefa345d9877f86ae5e798bdda2037101f54c
misaka18931 <misaka18931@protonmail.com>
2021-10-10 22:23:36 +0800

    OCTOBER 10th

---
+++ codeforces/educational-codeforces-round-115-div-2/a.cpp
@@ -0,0 +1,118 @@
+/**********************************************************************
+ * This file is the c++ solution to a particular CP problem written by
+ * misaka18931 and was hosted on GitHub Repository below:
+ * URL: https://github.com/misaka18931/competitive-programming
+ *
+ * Original Author: misaka18931
+ * Date:
+ * Algorithm:
+ * Difficulty:
+ *
+ *********************************************************************/
+
+#include <algorithm>
+#include <cctype>
+#include <climits>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+/********************************** buffer IO *********************************/
+namespace IO {
+char in[1 << 24];  // sizeof in varied in problem
+char const *o;
+void init_in() {
+  o = in;
+  in[fread(in, 1, sizeof(in) - 4, stdin)] = 0;  // set 0 at the end of buffer.
+}
+int rd() {
+  unsigned u = 0, s = 0;
+  while (*o && *o <= 32) ++o;  // skip whitespaces...
+  if (*o == '-')
+    s = ~s, ++o;
+  else if (*o == '+')
+    ++o;  // skip sign
+  while (*o >= '0' && *o <= '9')
+    u = (u << 3) + (u << 1) + (*o++ - '0');  // u * 10 = u * 8 + u * 2 :)
+  return static_cast<int>((u ^ s) + !!s);
+}
+char *rdstr(char *s) {
+  while (*o && *o <= 32) ++o;
+  while (*o > 32) *s++ = *o++;
+  *s = '\0';
+  return s;
+}
+}  // namespace IO
+
+/********************************* utility ************************************/
+typedef long long i64;
+typedef unsigned long long u64;
+typedef unsigned int u32;
+typedef pair<int, int> pii;
+#define pb(x) push_back(x)
+#define mkp(x, y) make_pair(x, y)
+#define fi first
+#define se second
+#define FOR(x, y, z) for (int x = y; x < z; ++x)  // always [y, z)
+#define ROF(x, y, z) for (int x = z - 1; x >= y; --x)
+template <typename T>
+void chkmax(T &a, const T &b) {
+  a = max(a, b);
+}
+template <typename T>
+void chkmin(T &a, const T &b) {
+  a = min(a, b);
+}
+pii operator+(const pii &a, const pii &b) {
+  return mkp(a.fi + b.fi, a.se + b.se);
+}
+
+/*********************************** solution *********************************/
+using IO::rd;
+#define MULTI
+const int N = 105;
+
+bool vis[2][N];
+char s[2][N];
+
+void solve() {
+  int n = rd();
+  FOR(i, 0, n) vis[0][i] = 0;
+  FOR(i, 0, n) vis[1][i] = 0;
+  IO::rdstr(s[0]);
+  IO::rdstr(s[1]);
+  vis[0][0] = 1;
+  if (s[1][0] == '0') vis[1][0] = 1;
+  FOR(i, 1, n) {
+    bool a = vis[1][i - 1] || vis[0][i - 1];
+    if (s[1][i] == '0') vis[1][i] = a;
+    if (s[0][i] == '0') vis[0][i] = a;
+  }
+  if (vis[1][n - 1]) cout << "YES\n";
+  else cout << "NO\n";
+}
+
+int main() {
+  IO::init_in();
+#ifdef MULTI
+  int T = IO::rd();
+  while (T--) solve();
+#else
+  solve();
+#endif
+  return 0;
+}
+/*
+ * checklist:
+ * - IO buffer size
+ * - potential out-of-bound Errors
+ * - inappropriate variable type
+ * - potential Arithmetic Error
+ * - potential Arithmetic Overflow
+ * - typo / logical flaws
+ * - clean-up on multiple test cases
+ * - sufficient stress tests / random data tests
+*/
---
+++ codeforces/educational-codeforces-round-115-div-2/c.cpp
@@ -0,0 +1,121 @@
+/**********************************************************************
+ * This file is the c++ solution to a particular CP problem written by
+ * misaka18931 and was hosted on GitHub Repository below:
+ * URL: https://github.com/misaka18931/competitive-programming
+ *
+ * Original Author: misaka18931
+ * Date:
+ * Algorithm:
+ * Difficulty:
+ *
+ *********************************************************************/
+
+#include <algorithm>
+#include <cctype>
+#include <climits>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+using namespace std;
+
+/********************************** buffer IO *********************************/
+namespace IO {
+char in[1 << 24];  // sizeof in varied in problem
+char const *o;
+void init_in() {
+  o = in;
+  in[fread(in, 1, sizeof(in) - 4, stdin)] = 0;  // set 0 at the end of buffer.
+}
+int rd() {
+  unsigned u = 0, s = 0;
+  while (*o && *o <= 32) ++o;  // skip whitespaces...
+  if (*o == '-')
+    s = ~s, ++o;
+  else if (*o == '+')
+    ++o;  // skip sign
+  while (*o >= '0' && *o <= '9')
+    u = (u << 3) + (u << 1) + (*o++ - '0');  // u * 10 = u * 8 + u * 2 :)
+  return static_cast<int>((u ^ s) + !!s);
+}
+char *rdstr(char *s) {
+  while (*o && *o <= 32) ++o;
+  while (*o > 32) *s++ = *o++;
+  *s = '\0';
+  return s;
+}
+}  // namespace IO
+
+/********************************* utility ************************************/
+typedef long long i64;
+typedef unsigned long long u64;
+typedef unsigned int u32;
+typedef pair<int, int> pii;
+#define pb(x) push_back(x)
+#define mkp(x, y) make_pair(x, y)
+#define fi first
+#define se second
+#define FOR(x, y, z) for (int x = y; x < z; ++x)  // always [y, z)
+#define ROF(x, y, z) for (int x = z - 1; x >= y; --x)
+template <typename T>
+void chkmax(T &a, const T &b) {
+  a = max(a, b);
+}
+template <typename T>
+void chkmin(T &a, const T &b) {
+  a = min(a, b);
+}
+pii operator+(const pii &a, const pii &b) {
+  return mkp(a.fi + b.fi, a.se + b.se);
+}
+
+/*********************************** solution *********************************/
+using IO::rd;
+#define MULTI
+const int N = 2e5+5;
+
+int a[N];
+map<int, int> c;
+
+void solve() {
+  c.clear();
+  int n = rd();
+  i64 s = 0;
+  FOR(i, 0, n) a[i] = rd(), s += a[i], ++c[a[i]];
+  if ((s * 2) % n) puts("0");
+  else {
+    sort(a, a + n);
+    int t = unique(a, a + n) - a;
+    s = s * 2 / n;
+    i64 ans = 0;
+    FOR(i, 0, t) {
+      if (a[i] < s - a[i]) ans += 1ll * c[a[i]] * c[s - a[i]];
+      if (a[i] == s - a[i]) ans += 1ll * c[a[i]] * (c[a[i]] - 1) / 2;
+    }
+    cout << ans << '\n';
+  }
+}
+
+int main() {
+  IO::init_in();
+#ifdef MULTI
+  int T = IO::rd();
+  while (T--) solve();
+#else
+  solve();
+#endif
+  return 0;
+}
+/*
+ * checklist:
+ * - IO buffer size
+ * - potential out-of-bound Errors
+ * - inappropriate variable type
+ * - potential Arithmetic Error
+ * - potential Arithmetic Overflow
+ * - typo / logical flaws
+ * - clean-up on multiple test cases
+ * - sufficient stress tests / random data tests
+*/
