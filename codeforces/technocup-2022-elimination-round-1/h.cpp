c928825b44805d48d47b8adbc68ac346f9f2ddca
misaka18931 <misaka18931@protonmail.com>
2021-10-18 23:26:32 +0800

    ok

--- codeforces/technocup-2022-elimination-round-1/h.cpp
+++ codeforces/technocup-2022-elimination-round-1/h.cpp
@@ -16,6 +16,7 @@
 #include <cstdio>
 #include <cstring>
 #include <iostream>
+#include <numeric>
 #include <string>
 #include <vector>
 using namespace std;
@@ -53,6 +54,7 @@ typedef unsigned long long u64;
 typedef unsigned int u32;
 typedef pair<int, int> pii;
 #define pb(x) push_back(x)
+#define eb emplace_back
 #define mkp(x, y) make_pair(x, y)
 #define fi first
 #define se second
@@ -73,10 +75,113 @@ pii operator+(const pii &a, const pii &b) {
 /*********************************** solution *********************************/
 using IO::rd;
 // #define MULTI
-const int N = 0;
+const int N = 2e5+5;
+vector<pii> g[N];
+int e[N], a[N], b[N], c[N], t[N];
+int n;
+int f[18][N], dep[N], fg[20][N];
+
+void dfs(int u, int fa) {
+  f[0][u] = fa;
+  for (auto [v, w] : g[u]) {
+    if (v == fa) continue;
+    dep[v] = dep[u] + 1;
+    fg[0][v] = w;
+    dfs(v, u);
+  }
+}
+
+inline void lca_init() {
+  dfs(1, 0);
+  for (int i = 1; i < 18; ++i) {
+    for (int j = 1; j <= n; ++j) {
+      f[i][j] = f[i-1][f[i-1][j]];
+      fg[i][j] = max(fg[i-1][j], fg[i-1][f[i-1][j]]);
+    }
+  }
+}
+
+int path(int u, int v) {
+  if (u == v) return 0;
+  int ret = 0;
+  if (dep[u] < dep[v]) swap(u, v);
+  int x = dep[u] - dep[v];
+  for (int i = 17; i >= 0; --i) {
+    if ((x >> i) & 1) chkmax(ret, fg[i][u]), u = f[i][u];
+  }
+  if (u == v) return ret;
+  for (int i = 17; i >= 0; --i) {
+    if (f[i][u] != f[i][v]) {
+      chkmax(ret, max(fg[i][u], fg[i][v]));
+      u = f[i][u];
+      v = f[i][v];
+    }
+  }
+  chkmax(ret, max(fg[0][u], fg[0][v]));
+  return ret;
+}
+
+struct DS {
+  int f[N], sz[N];
+  vector<int> mx[N];
+  int mxe[N], mxc[N];
+  inline void init() {
+    iota(f + 1, f + n + 1, 1);
+    FOR(i, 1, n + 1) mx[i].pb(i), mxe[i] = e[i], sz[i] = 1;
+  }
+  int get(int x) {
+    return (x == f[x]) ? x : (f[x] = get(f[x]));
+  }
+  void merge(int id) {
+    int u = get(a[id]), v = get(b[id]);
+    if (sz[u] < sz[v]) swap(u, v);
+    if (mxe[u] > mxe[v]) {
+    } else if (mxe[u] < mxe[v]) {
+      mxe[u] = mxe[v];
+      mxc[u] = mxc[v];
+      mx[u].swap(mx[v]);
+    } else {
+      for (auto t : mx[v]) {
+        chkmax(mxc[u], path(t, *mx[u].begin()));
+        mx[u].pb(t);
+      }
+    }
+    sz[u] += sz[v];
+    f[v] = u;
+    vector<int> stash;
+    mx[v].swap(stash);
+  }
+  pii qry(int x) {
+    int ft = get(x);
+    return {mxe[ft], max(mxc[ft], path(x, *mx[ft].begin()))};
+  }
+} D;
+
+int ord[N], ord2[N];
+int v[N], x[N];
+pii ans[N];
 
 void solve() {
-  
+  n = rd();
+  int q = rd();
+  FOR(i, 1, n + 1) e[i] = rd();
+  FOR(i, 1, n) {
+    a[i] = rd(), b[i] = rd(), c[i] = rd(), t[i] = rd();
+    g[a[i]].eb(b[i], t[i]);
+    g[b[i]].eb(a[i], t[i]);
+  }
+  lca_init();
+  D.init();
+  iota(ord + 1, ord + n, 1);
+  sort(ord + 1, ord + n, [] (int a, int b) { return c[a] > c[b]; } );
+  FOR(i, 0, q) v[i] = rd(), x[i] = rd();
+  iota(ord2, ord2 + q, 0);
+  sort(ord2, ord2 + q, [] (int a, int b) { return v[a] > v[b]; } );
+  for (int l = 1, i = 0; i < q; ++i) {
+    while (l < n && c[ord[l]] >= v[ord2[i]]) D.merge(ord[l++]);
+    ans[ord2[i]] = D.qry(x[ord2[i]]);
+  }
+  FOR(i, 0, q) cout << ans[i].fi << ' ' << ans[i].se << '\n';
 }
 
 int main() {
