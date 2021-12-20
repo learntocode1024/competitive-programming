d948f71898eef927e43899510806ecc2ff19102e
misaka18931 <misaka18931@protonmail.com>
2021-12-20 17:28:42 +0800

    ok

--- codeforces/educational-codeforces-round-119/f.cpp
+++ codeforces/educational-codeforces-round-119/f.cpp
@@ -47,11 +47,60 @@ inline void chkmax(T &a, const T b) {
   a = max(a, b);
 }
 
-//#define MULTI
-const int N = 0;
+#define MULTI
+const int N = 1e6+5;
+const int inf = 1e7;
+int n;
+int p[N], q[N];
+int f[N][2][2];
+int pre[N][2][2];
 
 inline void solve() {
-
+  rd(n);
+  FOR(i, 1, n + 1) rd(p[i]);
+  FOR(i, 1, n + 1) FOR(j, 0, 2) FOR(k, 0, 2) f[i][j][k] = inf;
+  f[1][0][0] = -inf;
+  f[1][1][0] = -inf;
+  FOR(i, 2, n + 1) {
+    for (int j = 0; j < 2; ++j, p[i] = -p[i]) {
+      int x, y;
+      for (int k = 0; k < 2; ++k, p[i - 1] = -p[i - 1]) {
+        for (int l = 0; l < 2; ++l) {
+          if (f[i-1][k][l] == inf) continue;
+          x = p[i-1];
+          y = f[i-1][k][l];
+          if (l) swap(x, y);
+          if (p[i] > x) {
+            if (y < f[i][j][0]) {
+              f[i][j][0] = y;
+              pre[i][j][0] = k << 1 | l;
+            }
+          }
+          else if (p[i] > y) {
+            if (x < f[i][j][1]) {
+              f[i][j][1] = x;
+              pre[i][j][1] = k<<1|l;
+            }
+          }
+        }
+      }
+    }
+  }
+  FOR(i, 0, 2) FOR(j, 0, 2) if (f[n][i][j] < inf) {
+    println("YES");
+    q[n] = p[n];
+    if (i) q[n] = -q[n];
+    ROF(t, 1, n) {
+      int pp = pre[t + 1][i][j];
+      i = pp >> 1;
+      j = pp & 1;
+      q[t] = p[t];
+      if (i) q[t] = -q[t];
+    }
+    FOR(s, 1, n + 1) cout << q[s] << " \n"[s==n];
+    return;
+  }
+  println("NO");
 }
 
 int main() {
@@ -79,3 +128,4 @@ int main() {
  * - memory usage
  * - file IO
  */
+
